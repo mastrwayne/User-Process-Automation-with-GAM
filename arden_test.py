c421ee91dfa515387f8b8c9a0e5581c8e702e156
Arden <>
2021-03-05 12:30:28 -0500

    added whitespace sensitivity to the script. also commented the script so it's more readable

--- arden_test.py
+++ arden_test.py
@@ -1,22 +1,34 @@
+#import useful thingssss
 import subprocess
 import os
 import time
 
+#prompt for username
 user = input("Enter user:")
 
+#remove any whitespace
+user = user.replace(" ","")
+
+#process username -> email
 user_full = user
 if "@" not in user:
     user_full+= "@adventures.org"
 
+#output user email
 print("User to process: "+user_full)
+
+#pause for effect
 time.sleep(1)
-print()
+print() #empty line
+
+#setup gam command variables
 gam_location = os.path.expanduser("~/bin/gamadv-xtd3/gam")
 gam_command = "whatis"
 gam_input = [gam_location,gam_command,user]
+
+#output gam command to be run
 print("Running Command: gam",gam_command,user)
 print()
 
-#list_files = os.popen(gam_input)
-
-list_files = subprocess.run(gam_input)
+#run gam command
+return_code = subprocess.run(gam_input)
